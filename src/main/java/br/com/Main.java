ffb1d1ec4626a3cd2091686e42c5ae369a84e28e
Gilvan Pedro <87739497+GilvanPedro@users.noreply.github.com>
2025-12-01 20:24:12 -0300

    Add files via upload

---
+++ src/main/java/br/com/Main.java
@@ -0,0 +1,11 @@
+package br.com;
+
+import br.com.view.TelaPrincipal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Inicia a interface gráfica do sistema
+        TelaPrincipal.main(args);
+    }
+}
